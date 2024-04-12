130726c8d29564c8a93825a511257ffcab08f69d
ecouus <166366430+ecouus@users.noreply.github.com>
2024-04-12 14:55:12 +0800

    Update ecouu.sh

--- ecouu.sh
+++ ecouu.sh
@@ -89,6 +89,7 @@ while true; do
     #echo -e "\033[38;5;208m |___ |___ |__| \033[0m"
     echo -e "\033[38;5;208m By Rational \033[0m"
     echo -e "\033[38;5;208m Blog：https://ecouu.com \033[0m"
+    echo "输入e即可召唤此脚本"
     echo " "
 
     echo "菜单栏："
