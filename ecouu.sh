d17da9b77f2a23d938e306f1d68ecb947d9f84da
ecouus <166366430+ecouus@users.noreply.github.com>
2024-04-12 15:03:48 +0800

    Update ecouu.sh

--- ecouu.sh
+++ ecouu.sh
@@ -90,6 +90,7 @@ while true; do
     echo -e "\033[38;5;208mAuthor：Rational \033[0m"
     echo -e "\033[38;5;208mBlog：https://ecouu.com \033[0m"
     echo "输入e即可召唤此脚本"
+    echo "------------------------"
     echo " "
 
     echo "菜单栏："
