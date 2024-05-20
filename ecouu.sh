0f974b5f66070a5250c04744736257a0c20d6770
ecouus <166366430+ecouus@users.noreply.github.com>
2024-05-20 21:54:36 +0800

    Update ecouu.sh

--- ecouu.sh
+++ ecouu.sh
@@ -109,6 +109,7 @@ while true; do
                 echo "5.sun-panel                    6.兰空图床"
                 echo "7.Filecodebox                  8.Wallos  "
                 echo "9.Linkding                     10.Alist    "
+                echo "11.Vocechat                       "
                 echo " "  
                 echo "0.返回主菜单   "
                 read -p "请输入你的选择：" choice
@@ -1071,6 +1072,97 @@ while true; do
                                         docker exec -it alist ./alist admin random
                                         echo " "
                                         echo "本地存储挂载路径（无需使用此项的可忽略）：-v /home/dc/alist/file:/opt/alist/data/file "
+                                        echo "脚本运行完毕"
+                                        # 提示用户按任意键继续
+                                        read -n 1 -s -r -p "按任意键返回"
+                                        echo  # 添加一个新行作为输出的一部分
+                                        ;;                   
+                                    2)
+                                        # 提示用户输入
+                                        echo "是否删除宿主机挂载卷 /home/dc/$name? (y/n)"
+                                        read answer
+                                        # 根据用户输入决定操作
+                                        case $answer in
+                                        y)
+                                            echo "Deleting..."
+                                            docker stop $name
+                                            docker rm $name
+                                            rm -rf /home/dc/$name
+                                            echo "Deleted."
+                                            ;;
+                                        n)
+                                            echo "Deleting..."  
+                                            docker stop $name
+                                            docker rm $name
+                                            echo "Docker项目已删除 挂载卷保留."
+                                            ;;
+                                        *)
+                                            echo "Invalid input. Please enter 'y' for yes or 'n' for no."
+                                            ;;
+                                        esac
+                                        read -n 1 -s -r -p "按任意键返回"
+                                        echo  # 添加一个新行作为输出的一部分
+                                        ;;
+                                    0)
+                                        eco
+                                        exit
+                                        ;;   
+                                    *)
+                                        echo "无效输入"
+                                        sleep 1
+                                        ;;
+                                esac
+                            done
+                            ;;
+                        11)
+                            while true; do
+                            clear
+                                echo -e "\033[38;5;208m'私人聊天室Vocechat' \033[0m"
+                                echo "源码：https://github.com/Privoce/vocechat-web"
+                                echo "------------------------"
+                                echo "菜单栏："
+                                echo "------------------------"
+                                echo "1.安装项目     2.删除项目"
+                                echo "0.返回主菜单"
+                                read -p "请输入你的选择：" user_choice
+                                name=vocechat-server
+                                port=3009
+                                case $user_choice in
+                                    1)
+                                        install_docker
+                                        iptables_open                     
+                                        # 检查名为sun-panel的容器是否存在
+                                        container_exists=$(docker ps -a --format '{{.Names}}' | grep -w "$name")
+                                        if [ "$container_exists" = "$name" ]; then
+                                            echo "已安装"
+                                        else
+                                            
+                                            # 初始化端口占用信息变量
+                                            ports_to_check=$port
+                                            # 使用函数检查定义的端口数组
+                                            if ! check_ports "${ports_to_check[@]}"; then
+                                                exit 1  # 如果检查失败则退出
+                                            else
+                                                echo "端口未被占用，可以继续执行"
+                                            fi
+
+                                        read -p $'\033[0;32m请输入域名(确保已反代至本机IP:3009):\033[0m' domain
+                                        full_domain="https://$domain"
+                                        docker pull privoce/vocechat-server:latest
+                                        docker run -d --restart=always \
+                                        -p 3009:3000 \
+                                        --name vocechat-server \
+                                        -v /home/dc/vocechat-server/data:/home/vocechat-server/data \
+                                        -e NETWORK__FRONTEND_URL="$full_domain" \
+                                        privoce/vocechat-server:latest
+
+                                        fi
+
+                                        clear
+                                        check_ip_address
+                                        echo "$name已搭建 "
+                                        echo "http://$ip_address:$port"
+                                        echo " "
                                         echo "脚本运行完毕"
                                         # 提示用户按任意键继续
                                         read -n 1 -s -r -p "按任意键返回"
